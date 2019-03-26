633251b5c9935a8059c8c29e059cd58ccb2e8e6b
rodubero <35814029+rodubero@users.noreply.github.com>
2019-03-26 19:43:53 +0200

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -12,13 +12,22 @@ def normalization(x, y, z):
     znorm = (z - np.min(z)) / (np.max(z) - np.min(z))
     return xnorm, ynorm, znorm
 
-def surface_plot(X,Y,Z):
-    #X = np.ravel(X)
-    #Y = np.ravel(Y)
-    #Z = np.ravel(Z)
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_trisurf(X, Y, Z, linewidth=0.5, antialiased=True)
+def surface_plot(X0, Y0, Z0, X1, Y1, Z1):
+    X0 = np.ravel(X0)
+    Y0 = np.ravel(Y0)
+    Z0 = np.ravel(Z0)
+    X1 = np.ravel(X1)
+    Y1 = np.ravel(Y1)
+    Z1 = np.ravel(Z1)
+    fig = plt.figure(figsize=(20,10))
+    ax = fig.add_subplot(1, 2, 1, projection='3d', adjustable='box')
+    #bx = fig.gca(projection='3d')
+    ax.plot_trisurf(X0, Y0, Z0, linewidth=0.5, antialiased=True)
+    ax.set_title("Original slope", fontweight='bold', fontsize=16, fontname='Arial', y=-0.05)
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax.plot_trisurf(X1, Y1, Z1, linewidth=0.5, antialiased=True)
+    ax.set_title("Selected slope", fontweight='bold', fontsize=16, fontname='Arial', y=-0.05)
+    plt.tight_layout()
     date = ti.strftime("%d_%m_%Y_%H_%M_%S")
     plt.savefig('Slope_'+date+'.png', bbox_inches='tight')
     plt.savefig('Slope.png', bbox_inches='tight')
@@ -32,7 +41,9 @@ def smoothness(Y):
     for i in range(Y.shape[1]):
         smoothness = np.average(np.gradient(Y[:,i]))
         smoothIndex.append(smoothness)
-    return np.average(smoothIndex)
+    smo=np.average(smoothIndex)
+    print(smo)
+    return smo
 
 def slopecalc(x, y, sensorpos):
     #Sensor position correction, sensor pos desceibes an offset coordinate
@@ -46,18 +57,17 @@ def slopecalc(x, y, sensorpos):
 
 if __name__ == '__main__':
     # Please provide surface data on 3 numpy arrays x, y and z
-    x = np.array([1, 1, 1, 2, 3, 4, 4, 4, 4, 1])
-    y = np.array([1, 2, 3, 3, 3, 3, 4, 5, 6, 1])
-    z = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    x = np.array([[1, 1, 1, 2, 3],[4, 4, 4, 4, 1]])
+    y = np.array([[1, 2, 3, 3, 3],[3, 4, 5, 6, 1]])
+    z = np.array([[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]])
     Y = np.array([[1, 1, 1, 2, 3, 4, 4, 4, 4, 1],[1, 2, 3, 3, 3, 3, 4, 5, 6, 1],[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
     # Defige sensor position to set reference frame on pole base (ground level)
     sensorpos = np.array([0, 0, 3])
     #x=np.reshape(x,())
-    #smo=smoothness(Y)
-    #print("Smoothness: ", smo)
-    #surface_plot(x,y,z)
+    smo = smoothness(Y)
+    surface_plot(x,y,z,x,y,z)
     # Calculate slope and smoothnes index from data provided
     slope = slopecalc(x, y, sensorpos)
 
     #NOTES:
-    #If the slope have steps or is uneven, please select each one and repeat the calculation process (line 29)
+    #If the slope have steps or is uneven, please select each one and repeat the calculation process (lines 67, 70)
